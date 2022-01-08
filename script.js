55459a17147d9b72509823b7c4756dd32c23f2b7
Tomin-Joy <tominjk007@gmail.com>
2022-01-08 17:36:38 +0530

    ^_~

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function calculate(){
     var t=document.forms["myForm"]["Total"].value;
     var p=document.forms["myForm"]["Percentage Required"].value/100;
     var res=(p*t - c)/(1-p)
-    document.getElementById("result").innerHTML=`You need ${res} more hrs for ${p*100}% attendence`;
-    console.log(`You need ${res} more hrs for ${p*100}% attendence`)
+    document.getElementById("result").innerHTML=`You need ${res} more hours class for ${p*100}% attendence`;
     return false;
 }
