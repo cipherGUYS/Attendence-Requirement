2dc1a26f9e5338e8759e49782431b1597e17b65c
Tomin-Joy <tominjk007@gmail.com>
2022-01-08 22:04:05 +0530

    ^_~

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function calculate(){
     if(p==0){
         p= (c/t) *100;
         p=Math.round(p*100)/100;
-        document.getElementById("result").innerHTML=`You alredy have ${p}% attendence`;    
+        document.getElementById("result").innerHTML=`You currently have ${p}% attendence`;    
     }
     else if((c/t)>p){
         document.getElementById("result").innerHTML=`You alredy have ${p*100}% attendence`;
