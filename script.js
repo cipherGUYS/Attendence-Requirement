2e81124edf38526084bbbd4a97f67f102fa7bc1f
Tomin-Joy <tominjk007@gmail.com>
2022-01-09 12:31:39 +0530

    ^_^

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ function calculate(){
     var c=document.forms["myForm"]["attendence"].value;
     var t=document.forms["myForm"]["Total"].value;
     var p=document.forms["myForm"]["Percentage Required"].value/100;
+    var cur = (c/t) *100;
+    cur = Math.round(cur*100)/100;
     if(p==0){
-        p= (c/t) *100;
-        p=Math.round(p*100)/100;
-        document.getElementById("result").innerHTML=`You currently have ${p}% attendence`;    
+        document.getElementById("result").innerHTML=`You currently have ${cur}% attendence`;    
     }
     else if((c/t)>p){
-        document.getElementById("result").innerHTML=`You alredy have ${p*100}% attendence`;
+        document.getElementById("result").innerHTML=`You alredy have more than ${p*100}% attendence`;
     }
     else{
         var res=(p*t - c)/(1-p)
         res = Math.ceil(res);
-        document.getElementById("result").innerText=`You need ${res} more hour class for ${p*100}% attendence`;
+        document.getElementById("result").innerHTML=`You currently have ${cur}% attendence.<br>You need ${res} more hour class for ${p*100}% attendence`;
         return false;
     }
 }
