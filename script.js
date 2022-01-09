14c1f1d7d4ce7c47900a1917b44e4296952752b4
Tomin-Joy <tominjk007@gmail.com>
2022-01-09 13:35:18 +0530

    😎

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ function calculate(){
     else if(c>t){
         document.getElementById("result").innerHTML=`${cur}% attendance 🙄 <br> Nee iluminanti aano? `;
     }
-    else if(p==1 && (cur)!=1){
-        document.getElementById("result").innerHTML=`100% onnum kitilla <br>Nee nadakan karyam vellom para`;
+    else if((p==100 && (cur)!=100) || p>100){
+        document.getElementById("result").innerHTML=`${p}% onnum kitilla <br>Nee nadakan karyam vellom para`;
     }
     else if(p==0){
         document.getElementById("result").innerHTML=`You currently have ${cur}% attendence`;    
     }
-    else if((cur)>=p){
-        document.getElementById("result").innerHTML=`You alredy have ${p}% attendence`;
+    else if((cur)=p){
+        document.getElementById("result").innerHTML=`You already have ${p}% attendence`;
     }
     else if((cur)>p){
-        document.getElementById("result").innerHTML=`You alredy have more than ${p}% attendence`;
+        document.getElementById("result").innerHTML=`You already have more than ${p}% attendence`;
     }
     else{
         var res=((p/100)*t - c)/(1-(p/100))
