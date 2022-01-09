f868128386f69ca5a0b8a951b2ea6377041807c4
Tomin-Joy <tominjk007@gmail.com>
2022-01-09 13:27:19 +0530

    😎

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 function calculate(){
     var c=document.forms["myForm"]["attendence"].value;
     var t=document.forms["myForm"]["Total"].value;
-    var p=document.forms["myForm"]["Percentage Required"].value/100;
+    var p=document.forms["myForm"]["Percentage Required"].value;
     var cur = (c/t) *100;
     cur = Math.round(cur*100)/100;
-    if(p==0){
+    if(t==0){
+        document.getElementById("result").innerHTML=`Vilachill edukaruthe keto`;
+    }
+    else if(c>t){
+        document.getElementById("result").innerHTML=`${cur}% attendance 🙄 <br> Nee iluminanti aano? `;
+    }
+    else if(p==1 && (cur)!=1){
+        document.getElementById("result").innerHTML=`100% onnum kitilla <br>Nee nadakan karyam vellom para`;
+    }
+    else if(p==0){
         document.getElementById("result").innerHTML=`You currently have ${cur}% attendence`;    
     }
-    else if((c/t)>p){
-        document.getElementById("result").innerHTML=`You alredy have more than ${p*100}% attendence`;
+    else if((cur)>=p){
+        document.getElementById("result").innerHTML=`You alredy have ${p}% attendence`;
+    }
+    else if((cur)>p){
+        document.getElementById("result").innerHTML=`You alredy have more than ${p}% attendence`;
     }
     else{
-        var res=(p*t - c)/(1-p)
+        var res=((p/100)*t - c)/(1-(p/100))
         res = Math.ceil(res);
-        document.getElementById("result").innerHTML=`You currently have ${cur}% attendence.<br>You need ${res} more hour class for ${p*100}% attendence`;
-        return false;
+        if (isNaN(res)) res = 0;
+        document.getElementById("result").innerHTML=`You currently have ${cur}% attendence.<br>You need ${res} more hour class for ${p}% attendence`;
     }
+    return false;
 }
