2d023b44c712e294b387e6c16c42faaf579c9817
Tomin-Joy <tominjk007@gmail.com>
2022-01-17 22:03:32 +0530

    (¬_¬ )

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
 
-      import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
-  import {getDatabase, ref, set, child, update, remove} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-database.js";
-  
-  const firebaseConfig = {
-    apiKey: "AIzaSyA67ON6yISRPHynNACqejaNz1cRP0wjib0",
-    authDomain: "cipherguys.firebaseapp.com",
-    databaseURL: "https://cipherguys-default-rtdb.firebaseio.com",
-    projectId: "cipherguys",
-    storageBucket: "cipherguys.appspot.com",
-    messagingSenderId: "807928981999",
-    appId: "1:807928981999:web:0c4519105736a80ac5ee1a",
-    measurementId: "G-RCE709EHL2"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
+import { getDatabase, ref, set, child, update, remove } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-database.js";
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const db = getDatabase();
-  function save(){
-    var c=document.forms["myForm"]["attendence"].value;
-    var t=document.forms["myForm"]["Total"].value;
-      set(ref(db,"hi/"+c),{
-          current : c,
-          tot : t
-      });
-  }
+const firebaseConfig = {
+    apiKey: "AIzaSyAt8Y0-eDgiXt7QRC3FkH6zr5mprYYPv9U",
+    authDomain: "attendence-e82cc.firebaseapp.com",
+    projectId: "attendence-e82cc",
+    storageBucket: "attendence-e82cc.appspot.com",
+    messagingSenderId: "143395028069",
+    appId: "1:143395028069:web:1546546fd0f5e57f585a8c"
+};
 
-function calculate(){
-    var c=document.forms["myForm"]["attendence"].value;
-    var t=document.forms["myForm"]["Total"].value;
-    var p=document.forms["myForm"]["Percentage Required"].value;
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+function save() {
+    var c = document.forms["myForm"]["attendence"].value;
+    var t = document.forms["myForm"]["Total"].value;
+    set(ref(db, "hi/" + c), {
+        current: c,
+        tot: t
+    });
+}
+
+function calculate() {
+    var c = document.forms["myForm"]["attendence"].value;
+    var t = document.forms["myForm"]["Total"].value;
+    var p = document.forms["myForm"]["Percentage Required"].value;
     save();
-    var cur = (c/t) *100;
-    cur = Math.round(cur*100)/100;
-    if(t==0){
-        document.getElementById("result").innerHTML=`Velachill edukaruthe keto`;
+    var cur = (c / t) * 100;
+    cur = Math.round(cur * 100) / 100;
+    if (t == 0) {
+        document.getElementById("result").innerHTML = `Velachill edukaruthe keto`;
     }
-    else if(cur>100){
-        document.getElementById("result").innerHTML=`${cur}% attendance 🙄 <br> Nee iluminanti aano? `;
+    else if (cur > 100) {
+        document.getElementById("result").innerHTML = `${cur}% attendance 🙄 <br> Nee iluminanti aano? `;
     }
-    else if((p==100 && (cur)!=100) || p>100){
-        document.getElementById("result").innerHTML=`${p}% onnum kitilla <br>Nee nadakana karyam vellom para`;
+    else if ((p == 100 && (cur) != 100) || p > 100) {
+        document.getElementById("result").innerHTML = `${p}% onnum kitilla <br>Nee nadakana karyam vellom para`;
     }
-    else if(p==0){
-        document.getElementById("result").innerHTML=`You currently have ${cur}% attendence`;    
+    else if (p == 0) {
+        document.getElementById("result").innerHTML = `You currently have ${cur}% attendence`;
     }
-    else if((cur)==p){
-        document.getElementById("result").innerHTML=`You already have ${p}% attendence`;
+    else if ((cur) == p) {
+        document.getElementById("result").innerHTML = `You already have ${p}% attendence`;
     }
-    else if((cur)>p){
-        document.getElementById("result").innerHTML=`You already have more than ${p}% attendence`;
+    else if ((cur) > p) {
+        document.getElementById("result").innerHTML = `You already have more than ${p}% attendence`;
     }
-    else{
-        var res=((p/100)*t - c)/(1-(p/100))
+    else {
+        var res = ((p / 100) * t - c) / (1 - (p / 100))
         res = Math.ceil(res);
         if (isNaN(res)) res = 0;
-        document.getElementById("result").innerHTML=`You currently have ${cur}% attendence.<br>You need ${res} more hour class for ${p}% attendence`;
+        document.getElementById("result").innerHTML = `You currently have ${cur}% attendence.<br>You need ${res} more hour class for ${p}% attendence`;
     }
     return false;
 }
 
 var cal = document.getElementById("Calc");
-cal.addEventListener("click",calculate);
+cal.addEventListener("click", calculate);
